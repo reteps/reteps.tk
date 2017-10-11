6f32e9f2e9aa557f5ddbd291744651e4fd8d0359
Peter Stenger <peter.a.stenger@gmail.com>
2017-10-11 17:46:10 -0400

    save

--- school/kahoot_test.php
+++ school/kahoot_test.php
@@ -13,9 +13,8 @@ if ($type == "bot") {
   $call = "~/www/reteps.tk/go/kahoot-auto " . $_GET['gamepin'] . " " . $_GET['username'] . " ";
   echo($call);
 }
-echo($username);
-echo($password);
-echo($loginUrl);
+echo($_GET['gamepin']);
+echo($_GET['username']);
 echo($kahootId);
 echo($type);
 ?>
