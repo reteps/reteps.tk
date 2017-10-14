e1c1365e8397d548494708ceff334d0c79202beb
Peter Stenger <peter.a.stenger@gmail.com>
2017-10-14 07:02:48 -0400

    save

--- php/xc_times.php
+++ php/xc_times.php
@@ -1,9 +1,10 @@
 <?php
 $SEARCH = $_GET['search'];
 $STATE = $_GET['state'];
+$SCHOOL = $_GET['school'];
 $RESULT = array();
 echo("<a href='https://reteps.tk/school/xc_times'>back</a><br>");
-exec('/home/retep/www/reteps.tk/exec/milesplit.py '.$SEARCH." ".$STATE, $RESULT);
+exec('/home/retep/www/reteps.tk/exec/milesplit.py '.$SEARCH." ".$STATE." ".$SCHOOL, $RESULT);
 foreach ($RESULT as $line) {
   echo($line."\n");
 }
